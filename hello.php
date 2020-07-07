1a03fdcfee773c84edc18bdfc272e43033941da3
thienth32 <thienth32@gmail.com>
2020-07-07 07:14:10 +0700

    update

---
+++ hello.php
@@ -0,0 +1,15 @@
+<?php
+$name = "thienth";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Xin chao toi ten là: <?php echo $name; ?></h2>
+</body>
+</html>
